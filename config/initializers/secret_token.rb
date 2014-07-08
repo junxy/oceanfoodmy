55acc56fb9505d7ade196ca726b3af782c8bc1ad
Junior <junior.xu@oceanwing.com>
2014-07-08 22:08:45 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oceanfoodmy::Application.config.secret_key_base = '21f18ea688099817b8a60a6fbc7197b69af7229943f7134a352b896f5ef6f54afe7597c5fe7539abb1215056e4f16dce75f58ae46f51f4860afa8934ee1e562d'
