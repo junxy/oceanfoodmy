3e6ae9a0b67fe8491723e48ec94624b31fb898ca
Junior <junior.xu@oceanwing.com>
2014-07-09 14:05:20 +0800

    配置 gem carrierwave 图片处理

--- app/uploaders/food_image_uploader.rb
+++ app/uploaders/food_image_uploader.rb
@@ -4,7 +4,7 @@ class FoodImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -31,10 +31,12 @@ class FoodImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  process :resize_to_fit => [500, 500]
+
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
